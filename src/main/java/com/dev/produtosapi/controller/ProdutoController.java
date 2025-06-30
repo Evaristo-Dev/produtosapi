0b2c3cd02cfc16632cf8380759a7461e38cad5b5
andre <andreevaristotavares@outlook.com>
2025-06-30 15:01:29 -0300

    deletar criado

--- src/main/java/com/dev/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/dev/produtosapi/controller/ProdutoController.java
@@ -2,12 +2,7 @@ package com.dev.produtosapi.controller;
 
 import java.util.UUID;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.dev.produtosapi.model.Produto;
 import com.dev.produtosapi.repository.ProdutoRepository;
@@ -31,12 +26,17 @@ public class ProdutoController {
     }
     @GetMapping("/listar")
     public Produto listarProdutos() {
-        return produtoRepository.findAll().get(0);
+        return produtoRepository.findAll().getFirst();
     }
 
     @GetMapping("/obter/{id}")
     public Produto obterProduto(@PathVariable String id) {
         return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public void deletar(@PathVariable("id") String id ){
+        produtoRepository.deleteById(id);
     }
 
 }
